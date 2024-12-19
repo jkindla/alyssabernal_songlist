ec789b7ea38e31641168c6bf774f33bd8375bba6
jkindla <82672312+jkindla@users.noreply.github.com>
2024-12-19 17:26:09 -0600

    edits

---
+++ script.js
@@ -0,0 +1,111 @@
+var notebooks = angular.module('notebooks', []);
+
+notebooks.controller('NotebookListCtrl', function($scope) {
+  $scope.notebooks = [
+    {"name": "Sunday Morning ",
+     "procesor": "Maroon 5",
+     "age": 2011},
+     {"name": "This Love",
+     "procesor": "Maroon 5",
+     "age": 2011},
+     {"name": "Killing Me Softly",
+     "procesor": "The Fugees",
+     "age": 2011},
+     {"name": "Dreams",
+     "procesor": "Fleetwood Mac",
+     "age": 2011},
+     {"name": "Valerie",
+     "procesor": "Amy Winehouse",
+     "age": 2011},
+     {"name": "Pony",
+     "procesor": "Ginuwine",
+     "age": 2011},
+     {"name": "Say my Name",
+     "procesor": "Destiny’s Child",
+     "age": 2011},
+     {"name": "Santeria",
+     "procesor": "Sublime",
+     "age": 2011},
+     {"name": "Como La Flor",
+     "procesor": "Selena",
+     "age": 2011},
+     {"name": "Amor Prohibido",
+     "procesor": "Selena",
+     "age": 2011},
+     {"name": "Ex's & Oh's",
+     "procesor": "Elle King",
+     "age": 2011},
+     {"name": "Superstition",
+     "procesor": "Stevie Wonder",
+     "age": 2011},
+     {"name": "Love on The Brain",
+     "procesor": "Rihanna",
+     "age": 2011},
+     {"name": "Flowers",
+     "procesor": "Miley Cyrus",
+     "age": 2011},
+     {"name": "Lose Control",
+     "procesor": "Teddy Swims",
+     "age": 2011},
+     {"name": "Best Part",
+     "procesor": "Daniel Caesar",
+     "age": 2011},
+     {"name": "Creep",
+     "procesor": "Radiohead",
+     "age": 2011},
+     {"name": "Zombie",
+     "procesor": "The Cranberries",
+     "age": 2011},
+     {"name": "Dance With Somebody",
+     "procesor": "Whitney Houston",
+     "age": 2011},
+     {"name": "Locked Out of Heaven",
+     "procesor": "Bruno Mars",
+     "age": 2011},
+     {"name": "Lazy Song",
+     "procesor": "Bruno Mars",
+     "age": 2011},
+     {"name": "No scrubs",
+     "procesor": "TLC",
+     "age": 2011},
+     {"name": "Umbrella",
+     "procesor": "Rihanna",
+     "age": 2011},
+     {"name": "Dont Speak",
+     "procesor": "No Doubt",
+     "age": 2011},
+     {"name": "Crazy",
+     "procesor": "Gnarles Barkley ",
+     "age": 2011},
+     {"name": "If I Ain’t Got You",
+     "procesor": "Alicia Keys",
+     "age": 2011},
+     {"name": "Pride and Joy",
+     "procesor": "Stevie Ray Vaughan",
+     "age": 2011},
+     {"name": "On & On",
+     "procesor": "Erykah Badu",
+     "age": 2011},
+     
+
+     
+
+  
+
+
+     
+     
+     
+     
+
+     
+
+     
+     
+
+
+     
+
+  ];
+  $scope.orderList = "name";
+});
